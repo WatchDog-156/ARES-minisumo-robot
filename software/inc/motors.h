703cbb219ef0a1d0e76c38e1b13789c9dabb47a5
jasiulfarba <278325@student.pwr.edu.pl>
2025-12-02 19:42:30 +0000

    added motors code

---
+++ software/inc/motors.h
@@ -0,0 +1,35 @@
+#ifndef MOTORS.H
+#define MOTORS.H
+
+#include "pico/stdlib.h"
+#include <cstdint>
+
+#define Left_Motor_PWM_Pin 12
+#define Left_Motor_Dir1_Pin 10
+#define Left_Motor_Dir2_Pin 11
+
+#define Right_Motor_PWM_Pin 14
+#define Right_Motor_Dir1_Pin 16
+#define Right_Motor_Dir2_Pin 15
+
+typedef enum{
+    Forward,
+    Backward,
+    Stop,
+} direction_t;
+
+typedef enum{
+    Left_Motor,
+    Right_Motor
+} motor_id_t;
+
+void motor_init(void);
+void motor_set(motor_id_t motor, direction_t direction, uint8_t speed);
+void motor_stop(motor_id_t motor);
+void all_motor_stop(void);
+
+
+
+
+
+#endif
