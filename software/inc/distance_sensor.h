32aa90e15935783987892814a82a86c1035756c9
jasiulfarba <278325@student.pwr.edu.pl>
2025-12-31 15:10:27 +0000

    distance_sensor work in progress

--- software/inc/distance_sensor.h
+++ software/inc/distance_sensor.h
@@ -1,5 +1,5 @@
-#ifndef DISTANCE_SENSOR.H
-#define DISTANCE_SENSOR.H
+#ifndef DISTANCE_SENSOR_H
+#define DISTANCE_SENSOR_H
 
 #include <stdint.h>
 
@@ -10,10 +10,13 @@
 #define XSHUT3_PIN 18
 #define XSHUT4_PIN 19
 
+extern uint8_t i2cAddress;
 void IR_init(void);
-int tofInit(int Chan, int Addr, int boolLongRange);
-int tofReadDistance(void);
-void tofSetAddress(uint8_t oldAddr, uint8_t newwAddr);
+int tofInit(int sensor_index, int Addr, int boolLongRange);
+int tofReadDistance(int sensor_index);
+void tofSetAddress(uint8_t oldAddr, uint8_t newAddr);
+void tofSetCurrentAddress(uint8_t addr);
+void tofSetCurrentSensor(int sensor_index);
 
 
 #endif
--- software/src/main.c
+++ software/src/main.c
@@ -6,26 +6,38 @@
 #include "bluetooth_connection.h"
 #include "motors.h"
 #include "serwo.h"
+#include "distance_sensor.h"
 
 static btstack_timer_source_t sensor_timer;
-
 static void sensor_timer_handler(btstack_timer_source_t *ts) {
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, !cyw43_arch_gpio_get(CYW43_WL_GPIO_LED_PIN));
-    
+    int dystans[4];
+    uint8_t addr[4]={0x31, 0x32, 0x33, 0x34};
+
     // Odczyt czujników
-    int status_left = read_detector_value(Left); 
-    int status_right = read_detector_value(Right); 
-    
+    int status_left = read_detector_value(Left);
+    int status_right = read_detector_value(Right);
+    for (int i=0; i<4; i++){
+        tofSetCurrentAddress(addr[i]);
+        dystans[i]=tofReadDistance(i);
+    }
+
     // Wysłanie telemetrii przez Bluetooth
     char msg[64];
     snprintf(msg, sizeof(msg), "L:%d R:%d\n", status_left, status_right);
     printf("%s", msg);
+    char msg2[128];
+
+    snprintf(msg2, sizeof(msg2), "XShut1: %d XShut2: %d XShut3: %d XShut4: %d\n", dystans[0], dystans[1], dystans[2], dystans[3]);
+    printf("%s", msg2);
     bluetooth_send_telemetry(msg);
+    bluetooth_send_telemetry(msg2);
 
     // Resetowanie timera na 500 ms
     btstack_run_loop_set_timer(ts, 500);
     btstack_run_loop_add_timer(ts);
 }
+
 
 #define UART_ID uart1
 #define BAUD_RATE 115200
@@ -34,17 +46,22 @@ static void sensor_timer_handler(btstack_timer_source_t *ts) {
 int main()
 {
     stdio_init_all();
-    
+    sleep_ms(30000);
+
     // Krótsze opóźnienie na start USB
+
     sleep_ms(3000);
     printf("=== MiniSumo Robot Starting ===\n");
-    
+
     // 1. Inicjalizacja CYW43 (WiFi/BT chip)
     if (cyw43_arch_init()) {
-        printf("ERROR: cyw43_arch_init failed!\n");
-        return -1;
+    printf("ERROR: cyw43_arch_init failed!\n");
+    return -1;
     }
+
     printf("CYW43 initialized OK\n");
+
+
 
     // 2. Włączenie LED
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
@@ -55,10 +72,16 @@ int main()
     printf("Line detectors initialized\n");
     motor_init();
     serwo_init();
+    IR_init();
+
+
+
 
     // 4. Inicjalizacja Bluetooth
     bluetooth_init();
     printf("Bluetooth initialized and advertising...\n");
+
+
 
     // 5. Konfiguracja timera dla czujników
     sensor_timer.process = &sensor_timer_handler;
@@ -69,45 +92,8 @@ int main()
     // 6. Start głównej pętli BTstack
     printf("Starting BTstack main loop...\n");
     printf("============================\n\n");
-    btstack_run_loop_execute(); 
+    btstack_run_loop_execute();
 
     // Ten kod nigdy się nie wykona
     return 0;
-}
-
-
-// int main() {
-//     stdio_init_all();
-    
-//     // Konfiguracja LED
-//     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-//     printf("System started. LED is ON.\n");
-
-//     /* linedetectors test*/
-//     /*line_detectors_init();
-//     while (true) {
-//         check_line_detectors();
-//         sleep_ms(500);
-//     }*/
-    
-//     /* IR sensors test*/
-//     /*if (!sensors_IR_init()) {
-//         printf("Błąd inicjalizacji czujników IR!\n");
-//         return -1;
-//     }
-//     while (1){
-//         for (uint8_t i = 0; i < 4; i++) {
-//             int16_t distance = read_IR_distance_mm(i);
-//             if (distance >= 0) {
-//                 printf("Czujnik %d: Odległość = %d mm\n", i, distance);
-//             } else {
-//                 printf("Czujnik %d: Błąd odczytu!\n", i);
-//             }
-//         }
-//         sleep_ms(1000);
-//     }*/
-    
-
-
-//     return 0;
-// }
+} 
