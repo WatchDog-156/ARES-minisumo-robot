417bd06bbc9ea871cb84843035d029cedb983639
jasiulfarba <278325@student.pwr.edu.pl>
2026-01-08 01:10:26 +0000

    starter

---
+++ software/inc/starter.h
@@ -0,0 +1,19 @@
+#ifndef STARTER_H
+#define STARTER_H
+
+#include <stdbool.h>
+
+#define Start_pin 14
+#define Kill_pin 15
+
+typedef enum{
+    Start,
+    Fighting,
+    End
+} State;
+
+void starter_init(uint Start_pin, uint Kill_pin);
+State getState(void);
+
+
+#endif
