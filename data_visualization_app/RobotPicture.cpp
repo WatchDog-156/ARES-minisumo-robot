d978456eba7031c06202f62329a02353bb0e48a7
WatchDog-156 <borsukiewiczkuba12345@gmail.com>
2026-03-29 20:48:28 +0200

    Dodano dokumentacje do klasy RobotPicture

--- data_visualization_app/RobotPicture.cpp
+++ data_visualization_app/RobotPicture.cpp
@@ -1,6 +1,21 @@
+/**
+ * @file RobotPicture.cpp
+ * @author Jakub Borsukiewicz (borsukiewiczkuba12345@gmail.com)
+ * @brief Klasa do zarządzania widget'em odpowiedzialnym za wyświetlanie zdjęcia robota MiniSumo
+ * @version 1.0
+ * @date 2026-03-29
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
 #include "RobotPicture.h"
 #include "ui_RobotPicture.h"
 
+/**
+ * @brief Construct a new Robot Picture:: Robot Picture object
+ * 
+ * @param parent wskażnik na rodzica obiektu
+ */
 RobotPicture::RobotPicture(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::RobotPicture)
@@ -8,6 +23,10 @@ RobotPicture::RobotPicture(QWidget *parent)
     ui->setupUi(this);
 }
 
+/**
+ * @brief Destroy the Robot Picture:: Robot Picture object
+ * 
+ */
 RobotPicture::~RobotPicture()
 {
     delete ui;
